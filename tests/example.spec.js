5073da9b993a60862d4f357b36eed6efd692c004
ventsislavVidulov <xcen3k@gmail.com>
2025-07-16 00:10:57 +0300

    test -  add before each statment for going on the test site page

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,11 +1,24 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('https://johakr.github.io/html5-slot-machine/');
+});
+
+const reelImagesUrls = [
+  {
+    name: 'Death Star',
+    url: 'https://johakr.github.io/html5-slot-machine/3392ebef20e51148368e.svg'
+  },
+];
+
+
+test.describe('start-up', () => {
+  test('page has title', async ({ page }) => {
+
+    await expect(page).toHaveTitle('HTML 5 Slot Machine');
 });
 
 test('get started link', async ({ page }) => {
