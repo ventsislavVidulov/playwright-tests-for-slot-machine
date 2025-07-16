11896771deea5a7e03f3f041067a0714088a9bd7
ventsislavVidulov <xcen3k@gmail.com>
2025-07-16 14:23:16 +0300

    util - add getSlotMachineState util function

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,7 +1,6 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
-
-
+import { log } from 'console';
 
 test.beforeEach(async ({ page }) => {
   await page.goto('https://johakr.github.io/html5-slot-machine/');
@@ -9,15 +8,46 @@ test.beforeEach(async ({ page }) => {
 
 const reelImagesUrls = [
   {
+    name: 'AT-AT',
+    url: 'https://johakr.github.io/html5-slot-machine/0379613a4269155f45b3.svg'
+  },
+  {
+    name: 'C-3PO',
+    url: 'https://johakr.github.io/html5-slot-machine/92e4eecf1c83293e3395.svg'
+  },
+  {
+    name: 'Darth Vader',
+    url: '	https://johakr.github.io/html5-slot-machine/9ae79499243776a4de6e.svg'
+  },
+  {
     name: 'Death Star',
     url: 'https://johakr.github.io/html5-slot-machine/3392ebef20e51148368e.svg'
   },
+  {
+    name: 'Millennium Falcon',
+    url: 'https://johakr.github.io/html5-slot-machine/b33cd68d7fb870c59309.svg'
+  },
+  {
+    name: 'R2-D2',
+    url: 'https://johakr.github.io/html5-slot-machine/a97a2e9fa184dcab972b.svg'
+  },
+  {
+    name: 'Stormtrooper',
+    url: '	https://johakr.github.io/html5-slot-machine/61e5b40f641dff097d3f.svg'
+  },
+  {
+    name: 'TIE Fighter',
+    url: 'https://johakr.github.io/html5-slot-machine/7d600c80a7f5f31bc337.svg'
+  },
+  {
+    name: 'Yoda',
+    url: 'https://johakr.github.io/html5-slot-machine/4c0ad8f5f10199effb77.svg'
+  }
 ];
 
 
 test.describe('start-up', () => {
   test('page has title', async ({ page }) => {
-
     await expect(page).toHaveTitle('HTML 5 Slot Machine');
   });
 
@@ -51,3 +81,35 @@ test.describe('start-up', () => {
     expect(jackpot).not.toBeNull();
   });
 });
+
+async function getSlotMachineState(page) {
+  //returns the state of the slot machine reels
+  const reels = await page.$$('#reels .reel');
+  const slotMachineState = [];
+  for (let i = 0; i < reels.length; i++) {
+    const reel = await reels[i].$$('img');
+    const reelState = [];
+    for (let j = 0; j < reel.length; j++) {
+      const img = reel[j];
+      const src = await img.getAttribute('src');
+      reelImagesUrls.find((item) => {
+        if (item.url === src) {
+          reelState.push(item);
+        }
+      });
+    }
+    slotMachineState.push(reelState);
+  }
+  return slotMachineState;
+}
+
+// test('test', async ({ page }) => {
+//   getSlotMachineState(page).then((state) => {
+//     for (let i = 0; i < state.length; i++) {
+//       log(`Reel ${i + 1}:`);
+//       for (let j = 0; j < state[i].length; j++) {
+//         log(`- ${state[i][j].name}`);
+//       }
+//     }
+//   });
+// });
